e13297d23f472d408c75dcd343fd7dbc988c9ffd
Your Name <you@example.com>
2025-04-27 11:27:06 -0600

    Actualizacion 2

--- Biblioteca/src/main/java/com/Integracion/Biblioteca/Mapper/EstudianteMapper.java
+++ Biblioteca/src/main/java/com/Integracion/Biblioteca/Mapper/EstudianteMapper.java
@@ -2,7 +2,7 @@ package com.Integracion.Biblioteca.Mapper;
 
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+
 
 import com.Integracion.Biblioteca.DTO.EstudianteDTO;
 import com.Integracion.Biblioteca.Entities.Estudiante;
